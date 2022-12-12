42b63754ae2fcaccac0773047b57ffb3bcd006ec
skandog <muumrar@gmail.com>
2022-12-12 15:45:08 +0000

    returned to this and its easy now

--- index.js
+++ index.js
@@ -1,22 +1,69 @@
+// function findStartOfPacketMarker(datastreamBuffer) {
+//   let lastFourCharacters = "";
+//   for (let i = 0; i < datastreamBuffer.length; i++) {
+//     if (lastFourCharacters.length > 4) {
+//       // remove the first character in the buffer
+//       lastFourCharacters = lastFourCharacters.substring(1);
+//     }
+//     lastFourCharacters += datastreamBuffer[i];
+//   }
+//   if (new Set(lastFourCharacters).size === 4) {
+//     // we found the start of the packet marker
+//     for (let j = 0; j < 4; j++) {
+//       if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
+//         console.log(lastFourCharacters);
+//       }
+//       console.log(lastFourCharacters);
+//       console.log(datastreamBuffer.indexOf(lastFourCharacters.charAt(j)));
+//     }
+//   }
+//   return -1;
+// }
+function count(word) {
+    var letters = {};
+    for (var _i = 0, word_1 = word; _i < word_1.length; _i++) {
+        var chr = word_1[_i];
+        letters[chr] = letters.hasOwnProperty(chr) ? letters[chr] + 1 : 1;
+    }
+    return letters;
+}
 function findStartOfPacketMarker(datastreamBuffer) {
     var lastFourCharacters = "";
+    console.log("datastreamBuffer", datastreamBuffer);
     for (var i = 0; i < datastreamBuffer.length; i++) {
-        if (lastFourCharacters.length > 4) {
-            // remove the first character in the buffer
-            lastFourCharacters = lastFourCharacters.substring(1);
+        lastFourCharacters = datastreamBuffer.slice(i, i + 4);
+        var letterCount = count(lastFourCharacters);
+        // console.log(Object.keys(letterCount).length);
+        if (Object.keys(letterCount).length > 3) {
+            console.log(count(lastFourCharacters));
+            console.log(i + 4);
+            console.log("found it sucker");
+            return;
         }
-        lastFourCharacters += datastreamBuffer[i];
-        if (new Set(lastFourCharacters).size === 4) {
-            // we found the start of the packet marker
-            for (var j = 0; j < 4; j++) {
-                if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
-                    console.log(lastFourCharacters);
-                }
-                console.log(lastFourCharacters);
-                console.log(datastreamBuffer.indexOf(lastFourCharacters.charAt(j)));
-            }
-        }
+        // for (let j = 0; j < 4; j++) {
+        //   console.log(
+        //     "j",
+        //     j,
+        //     lastFourCharacters.charAt(j),
+        //     lastFourCharacters,
+        //     lastFourCharacters.includes(lastFourCharacters.charAt(j))
+        //   );
+        //   if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
+        //     console.log("lastFourCharacters");
+        //   }
+        // }
+        // console.log(lastFourCharacters);
     }
+    // // we found the start of the packet marker
+    // for (let j = 0; j < 4; j++) {
+    //   if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
+    //     // console.log(lastFourCharacters);
+    //   }
+    //   console.log(lastFourCharacters);
+    //   //console.log(datastreamBuffer.indexOf(lastFourCharacters.charAt(j)));
+    // }
     return -1;
 }
-findStartOfPacketMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+// findStartOfPacketMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+// findStartOfPacketMarker("bvwbjplbgvbhsrlpgdmjqwftvncz");
+findStartOfPacketMarker("nppdvjthqldpwncqszvftbrmjlhg");
--- index.ts
+++ index.ts
@@ -1,23 +1,79 @@
+// function findStartOfPacketMarker(datastreamBuffer) {
+//   let lastFourCharacters = "";
+//   for (let i = 0; i < datastreamBuffer.length; i++) {
+//     if (lastFourCharacters.length > 4) {
+//       // remove the first character in the buffer
+//       lastFourCharacters = lastFourCharacters.substring(1);
+//     }
+//     lastFourCharacters += datastreamBuffer[i];
+//   }
+
+//   if (new Set(lastFourCharacters).size === 4) {
+//     // we found the start of the packet marker
+//     for (let j = 0; j < 4; j++) {
+//       if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
+//         console.log(lastFourCharacters);
+//       }
+//       console.log(lastFourCharacters);
+//       console.log(datastreamBuffer.indexOf(lastFourCharacters.charAt(j)));
+//     }
+//   }
+
+//   return -1;
+// }
+
+function count(word) {
+  let letters = {};
+  for (let chr of word) {
+    letters[chr] = letters.hasOwnProperty(chr) ? letters[chr] + 1 : 1;
+  }
+
+  return letters;
+}
+
 function findStartOfPacketMarker(datastreamBuffer) {
   let lastFourCharacters = "";
+  console.log("datastreamBuffer", datastreamBuffer);
   for (let i = 0; i < datastreamBuffer.length; i++) {
-    if (lastFourCharacters.length > 4) {
-      // remove the first character in the buffer
-      lastFourCharacters = lastFourCharacters.substring(1);
+    lastFourCharacters = datastreamBuffer.slice(i, i + 4);
+    let letterCount = count(lastFourCharacters);
+    // console.log(Object.keys(letterCount).length);
+
+    if (Object.keys(letterCount).length > 3) {
+      console.log(count(lastFourCharacters));
+      console.log(i + 4);
+      console.log("found it sucker");
+      return;
     }
-    lastFourCharacters += datastreamBuffer[i];
-    if (new Set(lastFourCharacters).size === 4) {
-      // we found the start of the packet marker
-      for (let j = 0; j < 4; j++) {
-        if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
-          console.log(lastFourCharacters);
-        }
-        console.log(lastFourCharacters);
-        console.log(datastreamBuffer.indexOf(lastFourCharacters.charAt(j)));
-      }
-    }
+
+    // for (let j = 0; j < 4; j++) {
+    //   console.log(
+    //     "j",
+    //     j,
+    //     lastFourCharacters.charAt(j),
+    //     lastFourCharacters,
+    //     lastFourCharacters.includes(lastFourCharacters.charAt(j))
+    //   );
+    //   if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
+    //     console.log("lastFourCharacters");
+    //   }
+    // }
+    // console.log(lastFourCharacters);
   }
+
+  // // we found the start of the packet marker
+  // for (let j = 0; j < 4; j++) {
+  //   if (!lastFourCharacters.includes(lastFourCharacters.charAt(j))) {
+  //     // console.log(lastFourCharacters);
+  //   }
+  //   console.log(lastFourCharacters);
+  //   //console.log(datastreamBuffer.indexOf(lastFourCharacters.charAt(j)));
+  // }
+
   return -1;
 }
 
-findStartOfPacketMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+// findStartOfPacketMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+
+// findStartOfPacketMarker("bvwbjplbgvbhsrlpgdmjqwftvncz");
+findStartOfPacketMarker("nppdvjthqldpwncqszvftbrmjlhg");
